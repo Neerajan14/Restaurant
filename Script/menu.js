e43ade463a33bac3df2aa214601217ed793bd86e
Neerajan <nirajanbohora@gmail.com>
2026-07-30 15:55:58 +0545

    menu page

---
+++ Script/menu.js
@@ -0,0 +1,34 @@
+// Load Navbar
+fetch("nav.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+// Load Footer
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
+
+
+    $(function () {
+
+    $("#booking-date").datepicker({
+        dateFormat: "dd-mm-yy",
+        minDate: 0
+    });
+
+    $("#booking-time").timepicker({
+        timeFormat: "h:mm p",
+        interval: 30,
+        minTime: "10:00am",
+        maxTime: "10:00pm",
+        defaultTime: "11:00am",
+        dynamic: false,
+        dropdown: true,
+        scrollbar: true
+    });
+
+});
