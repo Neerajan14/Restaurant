2926701780e80578383d3d721702d4334d5386a0
Neerajan <nirajanbohora@gmail.com>
2026-08-05 12:08:08 +0545

    updating

--- Script/reservation.js
+++ Script/reservation.js
@@ -14,45 +14,36 @@ fetch("footer.html")
 
 
 
-    $(document).ready(function () {
+$(document).ready(function () {
 
     // Date Picker
     $("#datepicker").datepicker({
-
         dateFormat: "dd-mm-yy",
-
         minDate: 0,
-
         changeMonth: true,
-
         changeYear: true
-
     });
-
 
     // Time Picker
     $("#timepicker").timepicker({
-
         timeFormat: "h:mm p",
-
         interval: 30,
-
         minTime: "10:00am",
-
         maxTime: "10:00pm",
-
         defaultTime: "11:00am",
-
         startTime: "10:00",
-
         dynamic: false,
-
         dropdown: true,
-
         scrollbar: true
+    });
 
+    // Select2
+    $("#guest-select").select2({
+        minimumResultsForSearch: Infinity,
+        width: "100%"
     });
 
 });
 
+ 
 
