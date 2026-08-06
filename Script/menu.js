726c67c68eea9a58efb4b3c49875b340f61ba799
Neerajan <nirajanbohora@gmail.com>
2026-08-06 12:34:29 +0545

    updating

--- Script/menu.js
+++ Script/menu.js
@@ -22,12 +22,7 @@ fetch("footer.html")
         document.getElementById("footer").innerHTML = data;
     });
 
-// Load Footer
-fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data;
-    });
+
 
 
     $(function () {
